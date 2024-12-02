9f76fe03c792f4ca81c621f547fb8b2b853b98fc
asitshakya678 <asitshakya789@gmail.com>
2024-12-03 00:46:31 +0530

    Problem of 14th day

---
+++ Day_14_Sort_An_Array_of_0s_1s_and_2s.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Day_14_Sort_An_Array_of_0s_1s_and_2s {
+
+    
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        Collections.sort(arr);
+    }
+}
